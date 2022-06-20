965c8fd12e6f82b9b685b07a42819533ff2c0879
Deepred <deepred.kim@gmail.com>
2022-06-20 13:38:53 +0900

    feat: Spring Cloud Gateway 단순세팅 완료
    /api /apiRider에 대해 localhost:8080으로 라우팅

--- src/main/java/kr/flab/momukji/gateway/GatewayApplication.java
+++ src/main/java/kr/flab/momukji/gateway/GatewayApplication.java
@@ -2,12 +2,27 @@ package kr.flab.momukji.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p
+				.path("/api")
+				.uri("http://localhost:8080/"))
+			.route(p -> p
+				.path("/apiRider")
+				.uri("http://localhost:8080/"))
+			.build();
 	}
 
 }
