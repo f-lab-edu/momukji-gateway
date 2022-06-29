12f7db808eb152d1abc6d2fb50d65c2666fe6dc2
sue4869 <sue4869@naver.com>
2022-06-29 19:35:32 +0900

    feat : Eureka Server 적용
    * 폴더 구성 재구조화 (gateway 프로젝트, Eureka Server 프로젝트)
    * Eureka Server 구축완료
    * gateway프로젝트에 Eureka Client 적용완료

---
+++ eurekaServer/src/main/java/kr/flab/momukji/eurekasever/EurekaseverApplication.java
@@ -0,0 +1,15 @@
+package kr.flab.momukji.eurekasever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaseverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaseverApplication.class, args);
+	}
+
+}
