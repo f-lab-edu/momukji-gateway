b43e5484a3cdb826698e1b0fa6967ec12f5309b2
sue4869 <sue4869@naver.com>
2022-06-29 21:40:59 +0900

    refator: 오타 수정 및 gitignore 수정

--- eurekaServer/src/main/java/kr/flab/momukji/eurekaserver/EurekaseverApplication.java
+++ eurekaServer/src/main/java/kr/flab/momukji/eurekaserver/EurekaseverApplication.java
@@ -1,4 +1,4 @@
-package kr.flab.momukji.eurekasever;
+package kr.flab.momukji.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
